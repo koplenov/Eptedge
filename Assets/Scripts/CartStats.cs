a3834b3da16c5c6fa0b12ded182a0fad2f48dabb
gerak24 <gerak24032003@yandex.ru>
2022-06-02 10:38:25 +0300

    Stats

---
+++ Assets/Scripts/CartStats.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CartStats
+{
+public static int Durability { get; set; }
+public static int Cargo { get; set; }
+public static int JewishModifier { get; set; }
+
+public static int MaxDurability { get; set; }
+public static int MaxCargo { get; set; }
+public static int MaxJewishModifier { get; set; }
+}
